d047b6495a9e6af778d2a215344eca4d086ed74e
Jesus Luque <jluque@soccerfactory.com>
2018-11-02 12:40:20 +0100

    cambios en el proyecto

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-echo "Pagina de prueba";
-
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+		<?php
+		   echo "pagina de prueba";
+		?>
+    </body>
+</html>
